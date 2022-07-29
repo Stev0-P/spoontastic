a7909386693f4eb0918ff97a68e52c9ab5956ba6
unknown <steven.pahyrya@wearenova.co.uk>
2022-07-29 12:38:21 +0100

    Screens Updated with boilerplate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
 
+
+
 const App = () => (
   <Switch>
     <Route exact={true} path="/" component={Home} />
