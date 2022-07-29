6318d3dbdf81063cd6c3a0cf6e93f34327e24c5f
Oran Doherty <oran.doherty@wearenova.co.uk>
2022-07-29 15:05:21 +0100

    preferences.js

--- src/screens/preferences.js
+++ src/screens/preferences.js
@@ -1,8 +1,28 @@
 import React from "react";
-
+import Container from '@mui/material/Container';
 
 const Preferences = () => {
-    return <div>Preferences</div>
+    const styler = {
+        display: 'flex',
+        alignItems: 'left',
+        justifyContent: '',
+        height: '40vh',
+    };
+
+    return (
+        <Container maxWidth="sm">
+        <div style={styler}>
+            <div>
+                <h1>Welcome</h1>
+                <br />
+                <h1>Please Select Your Dietry Preferences</h1>
+            </div>
+        </div>
+        <hr />
+        <hr />
+        </Container>
+
+    );
 }
 
 export default Preferences;
