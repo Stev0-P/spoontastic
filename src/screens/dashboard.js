0d02cb3525719f9b69535e6f1dc66be5638cb930
unknown <steven.pahyrya@wearenova.co.uk>
2022-08-04 09:58:09 +0100

    error handling

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -2,13 +2,13 @@ import React from "react";
 import RecipeList from "../components/RecipeList";
 import Container from "@mui/material/Container";
 import DashboardWidgets from "../components/DashboardWidgets";
+import { Box } from "@mui/system";
 
 const Dashboard = () => {
   return (
-    <Container>
-      <DashboardWidgets />
+    <Box>
       <RecipeList label="Reccomended" />
-    </Container>
+   </Box>
   );
 };
 
