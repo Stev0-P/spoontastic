f3039385088968dcc57a96fb3b2d0e7bcc0e7587
Oran Doherty <oran.doherty@wearenova.co.uk>
2022-08-04 09:27:41 +0100

    Dashboard Updates

--- src/components/DashboardWidgets.js
+++ src/components/DashboardWidgets.js
@@ -2,48 +2,87 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import { useHistory, useLocation } from "react-router-dom";
 
 const DashboardWidgets = () => {
   const tempROTD = {
-    description: "Pizza",
+    description: "Lemon Herb Grilled Salmon",
     img: "https://images.unsplash.com/photo-1619734490039-a68d5c82cf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&q=80",
     route: "/recipe:id",
   };
 
   const RecipeOfTheDay = () => {
+    const history = useHistory();
+
     return (
       <Container
         sx={{
-          flexGrow: 1,
-          backgroundColor: "#f7a05e",
           borderRadius: "2em",
-          width: "200px"
+          width: "200px",
+          display: "flex",
+          flexGrow: 1,
+          flexDirection: "column",
+          backgroundColor: "#f7a05e",
+          marginRight: 4,
+          marginLeft: 3,
+          marginTop: 3,
+          paddingTop: "1em",
+          boxShadow: 3,
         }}
+        onClick={() => history.push("/recipe/:id")}
       >
-        <Typography
-          sx={{display: "flex", justifyContent: "centre", mt: 4, mb: 2}}
-          variant="h6"
-          component="div"
-        >
+        <Typography sx={{ display: "flex" }} variant="h6" component="div">
           Recipe Of The Day
         </Typography>
-        <div>
-          <img
-            style={{
-              height: "125px",
-              width: "150px",
+        <Box
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            flexDirection: "column",
+            backgroundColor: "#f7a05e",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "#f5efe9",
               borderRadius: "1em",
+              borderTop: "1em",
+              width: "200px",
             }}
-            src={tempROTD.img}
-          />
-          <Typography
-            sx={{display: "flex", justifyContent: "centre", mt: 4, mb: 2, marginLeft: 1, marginTop: 1, backgroundColor: '#f5efe9', borderRadius: "1em", marginTop: 1, boxShadow: 2}}
-            variant="h7"
-            component="div"
           >
-            {tempROTD.description}
-          </Typography>
-        </div>
+            <Typography variant="body1">
+              <Box>
+                <img
+                  style={{
+                    height: "125px",
+                    width: "150px",
+                    borderRadius: "1em",
+                  }}
+                  src={tempROTD.img}
+                />
+                <Typography
+                  sx={{
+                    display: "flex",
+                    justifyContent: "centre",
+                    mt: 4,
+                    mb: 2,
+                    backgroundColor: "#f5efe9",
+                    marginTop: 1,
+                    boxShadow: 2,
+                    borderRadius: "1em",
+                    paddingTop: "1em",
+                    paddingLeft: "1em",
+                    paddingBottom: "1.5em",
+                  }}
+                  variant="h7"
+                  component="div"
+                >
+                  {tempROTD.description}
+                </Typography>
+              </Box>
+            </Typography>
+          </Box>
+        </Box>
         ;
       </Container>
     );
--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import RecipeList from "../components/RecipeList";
-
+import Container from "@mui/material/Container";
+import DashboardWidgets from "../components/DashboardWidgets";
 
 const Dashboard = () => {
-    return (
-    <RecipeList label="Reccomended" />
-    );
-}
+  return (
+    <Container>
+      <DashboardWidgets />
+      <RecipeList label="Reccomended" />
+    </Container>
+  );
+};
 
 export default Dashboard;
