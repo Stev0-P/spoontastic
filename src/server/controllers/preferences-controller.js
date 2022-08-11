2bbe35bd938b8e1428cd45816d83045228a9823b
Oran Doherty <oran.doherty@wearenova.co.uk>
2022-08-11 13:12:17 +0100

    Finished preferences controller and route

--- src/server/controllers/preferences-controller.js
+++ src/server/controllers/preferences-controller.js
@@ -1,25 +1,71 @@
+import { v4 as uuid4 } from "uuid";
+
 const DUMMY_PREFERENCES = [
-    {
-        id: 1,
-        userId: 1,
-        diet: 'Gluten Free',
-        intolerance: ('Egg', 'Gluten', 'Grain'),
-        time: 'Breakfast'
-    }
+  {
+    id: "1",
+    userId: "1",
+    diet: "Gluten Free",
+    intolerance: ["egg", "grain"],
+    type: ["breakfast", "Snacks"],
+  },
+  {
+    id: "2",
+    userId: "1",
+    diet: "Vegan",
+    intolerance: ["egg", "soya", "dairy"],
+    type: ["main course", "Snacks"],
+  }
 ];
 
-const createPreference = (req, res, next) => {
-    const { diet, intolerance, time } = req.body;
+const createPreferences = (req, res, next) => {
+  const { diet, intolerance, type} = req.body;
 
-    const createdPreference = {
-        diet,
-        intolerance,
-        time
-    };
+  const createdPreferences = {
+    id: uuid4(),
+    diet,
+    intolerance,
+    type,
+  };
 
-    DUMMY_PREFERENCES.push(createdPreference);
+  DUMMY_PREFERENCES.push(createdPreferences);
 
-    res.status(201).json({preferance: createdPreference})
+  res.status(201).json({ preferances: createdPreferences });
 };
 
-exports.createPreferences = createPreferences;
+const getPreferencesById = (req, res, next) => {
+  const preferencesID = req.params.pid; //{}
+  const preferences = DUMMY_PREFERENCES.find((p) => {
+    return p.id === preferencesID;
+  });
+  if (!preferences) {
+    const error = new Error(
+      "Could not find a preference with the provided id."
+    );
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ preferences: preferences });
+};
+
+const updatePreferences = (req, res, next) => {
+  const { diet, intolerance, type } = req.body
+  const preferencesId = req.params.pid;
+
+  const updatedPreferences = { ...DUMMY_PREFERENCES.find(p => p.id === preferencesId)};
+  const preferencesIndex = DUMMY_PREFERENCES.findIndex(p => p.id === preferencesId);
+  if (diet) updatedPreferences.diet = diet;
+  if (intolerance) updatedPreferences.intolerance = intolerance;
+  if (type) updatedPreferences.type = type;
+
+  DUMMY_PREFERENCES[preferencesIndex] = updatedPreferences;
+
+  res.status(200).json({ preferences: updatedPreferences })
+}
+
+const _createPreferences = createPreferences;
+const _getPreferencesById = getPreferencesById;
+const _updatePreferences = updatePreferences;
+
+export { _createPreferences as createPreferences };
+export { _getPreferencesById as getPreferencesById };
+export { _updatePreferences as updatePreferences };
--- src/server/routes/preferences.js
+++ src/server/routes/preferences.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import CheckBox from "../../client/components/checkbox";
 
-const preferencesControllers = require('../controllers/preferences');
+const preferencesControllers = require("../controllers/preferences-controller");
 
 const router = Router();
 
-router.post('/', preferencesControllers.createPreference);
+router.get("/preferences/:pid", preferencesControllers.getPreferencesById);
+
+router.post("/", preferencesControllers.createPreferences);
+
+router.patch("/:pid", preferencesControllers.updatePreferences);
 
 export default router;
