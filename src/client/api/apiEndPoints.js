b0e5feca19e80ab27882c678e1caad2e8bf6ad48
unknown <steven.pahyrya@wearenova.co.uk>
2022-08-12 12:27:37 +0100

    client api end points

---
+++ src/client/api/apiEndPoints.js
@@ -0,0 +1,9 @@
+export const API_ENDPOINTS = {
+  LOGIN: "/public/authentication/login",
+  LOGOUT: "/public/authentication/logout",
+  RECIPE_LIST: "/recipes",
+  FAVOURITES_LIST: "/favourites",
+  ADD_TO_FAVOURITES: "/favourites/addToFav",
+  DELETE_FAVOURITE: "/favourites/delete",
+  USER_PREFERENCES: "/preferences",
+};
