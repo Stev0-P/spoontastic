bc73531516da369513299dad62a29f2d124fc66d
Oran Doherty <oran.doherty@wearenova.co.uk>
2022-08-12 11:37:19 +0100

    Added functions to accountAPI

--- src/server/api/accountAPI.js
+++ src/server/api/accountAPI.js
@@ -1,5 +1,44 @@
+import { v4 as uuid4 } from "uuid";
 import { Router } from "express";
 
 const accountAPI = Router();
+
+const DUMMY_USERS = [
+  {
+    id: "1",
+    name: "Oran Doherty",
+    email: "oran.doherty@wearenova.co.uk",
+  },
+];
+
+accountAPI.get("/getUsers", (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+});
+
+accountAPI.post("/createUser", (req, res, next) => {
+  const { name, email } = req.body;
+
+  const createdUser = {
+    id: uuid4(),
+    name: name,
+    email: email,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+});
+
+accountAPI.post("/login", (req, res, next) => {
+  const { email } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser) {
+    const error = new Error("Could not identify user, credentials seem to be wrong.");
+    error.code = 401;
+    return next(error);
+  }
+  res.json({ message: "Logged in!" })
+});
 
 export default accountAPI;
