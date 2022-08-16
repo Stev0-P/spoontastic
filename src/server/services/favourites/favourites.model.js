7e6d4a4e553eace78fee0425a805357a96aee64e
unknown <steven.pahyrya@wearenova.co.uk>
2022-08-16 13:41:30 +0100

    Connected to mongoDb, adding and retrieveng favourites recipes

--- src/server/services/favourites/favourites.model.js
+++ src/server/services/favourites/favourites.model.js
@@ -1,8 +1,9 @@
-import mongoose, { Schema, Dosument, LeanDocument } from "mongoose";
+import mongoose, { Schema, Document, LeanDocument } from "mongoose";
 
 const favouritesListItemSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String, required: true },
+  creator: { type: String, required: true },
 });
 
 export default mongoose.model("Favourites", favouritesListItemSchema);
