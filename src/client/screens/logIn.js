7d6c7b00a73defc816af843621b798b576348eaa
Oran Doherty <oran.doherty@wearenova.co.uk>
2022-08-16 09:58:25 +0100

    Styling for login

--- src/client/screens/logIn.js
+++ src/client/screens/logIn.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 export const LogIn = () => {
   function handleCallbackResponse(response) {
@@ -11,8 +11,7 @@ export const LogIn = () => {
     script.onload = () => {
       /* global google */
       google.accounts.id.initialize({
-        client_id:
-          "310796731049-go21cqbgntnb8ld42p9m5gm4odh78487.apps.googleusercontent.com",
+        client_id: "310796731049-go21cqbgntnb8ld42p9m5gm4odh78487.apps.googleusercontent.com",
         callback: handleCallbackResponse,
       });
 
@@ -26,8 +25,69 @@ export const LogIn = () => {
   }, []);
 
   return (
-    <Box>
-      <div className="signInDiv"></div>
-    </Box>
+      <Container className="signInDiv">
+        <Box
+          className="WelcomeBox1"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "30vh",
+            fontSize: "1.5em",
+            textAlign: "center",
+            padding: "em",
+            flexGrow: 1,
+            backgroundColor: "#f7a05e",
+            borderRadius: "1em",
+            margin: 2,
+          }}
+        >
+          <Box
+            className="WelcomeBox2"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "25vh",
+              padding: "em",
+              flexGrow: 1,
+              backgroundColor: "#81c784",
+              borderRadius: "1em",
+              margin: "3vh",
+            }}
+          >
+            <Box
+              className="WelcomeBox3"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "2em",
+                height: "20vh",
+                textAlign: "center",
+                padding: "em",
+                flexGrow: 1,
+                backgroundColor: "#fff",
+                borderRadius: "1em",
+                margin: "3vh",
+              }}
+            >
+              Welcome to Spoontastic
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          style={{
+            display: "flex",
+            alignItems: "center",
+            textAlign: "center",
+            justifyContent: "center",
+            padding: "6em",
+            fontSize: "1.5em",
+          }}
+        >
+          Login with Google to access a world of spoontastic recipes!
+        </Box>
+      </Container>
   );
 };
