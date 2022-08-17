37be87f22408de7c0b8ad00ee94c9e6b15782fe9
Oran Doherty <oran.doherty@wearenova.co.uk>
2022-08-17 14:53:30 +0100

    Linking user ID to favourites

--- src/server/services/users/user.model.js
+++ src/server/services/users/user.model.js
@@ -4,10 +4,11 @@ import uniqueValidator from "mongoose-unique-validator";
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  preferences: { type: String, required: true },
-  JWT: { type: String, required: true }
+  preferences: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Preferences' },
+  JWT: { type: String, required: true },
+  favourites: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Favourites' }]
 });
 
 userSchema.plugin(uniqueValidator);
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model('User', userSchema);
