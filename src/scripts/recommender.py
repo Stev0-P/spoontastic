c24b74a5394a98699f8b330fbd0c6ac5f5bd77f9
Stev0-P <steven011506@gmail.com>
2023-04-03 17:04:31 +0100

    alot of updates, on reccomender, saving data in mongo db through python file, fetching datat into the endpoint in node, and fetching data in front end ti display user reccomended

--- src/scripts/recommender.py
+++ src/scripts/recommender.py
@@ -1,82 +1,137 @@
 import pandas as pd
 import numpy as np
 import pymongo
+import pprint
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
 
+
+# Step 1: Collect data
+# Connect To MongoDB DB and extract user ratings into a matrix of recipes and ratings made by users
 myclient = pymongo.MongoClient(
     'mongodb://admin:password@localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256')
 
 db = myclient.test
 
 collection = db.ratings
+collection2 = db.users
+collection3 = db.recommended
 
-print(db.list_collection_names())
+
+x = collection.find({}, {"_id": 0, "creator": 1, "rating": 1, "recipeID": 1})
 
 
-# Step 1: Collect data
-# Create a ratings matrix with 5 users and 5 recipes
-ratings_data = {'user1': [3, 2, 0, 4, 5, 0, 4, 2, 0, 0, 0, 3, 4],
-                'user2': [1, 4, 2, 3, 0, 1, 3, 0, 5, 2, 0, 0, 0],
-                'user3': [2, 3, 4, 0, 1, 0, 1, 2, 0, 3, 0, 0, 3],
-                'user4': [0, 3, 2, 4, 0, 2, 2, 0, 3, 5, 0, 3, 0],
-                'user5': [5, 2, 3, 0, 4, 3, 4, 3, 4, 2, 0, 0, 2]}
+r_data = pd.DataFrame(list(x))
 
-ratings_df = pd.DataFrame(ratings_data, index=['recipe1', 'recipe2', 'recipe3', 'recipe4', 'recipe5',
-                          'recipe6', 'recipe7', 'recipe8', 'recipe9', 'recipe10', 'recipe11', 'recipe12', 'recipe13'])
+pivot_df = pd.pivot_table(r_data, values='rating',
+                          index='recipeID', columns='creator')
+
+pivot_df = pivot_df.fillna(0)
 
 
 # Step 2: Compute similarity
 # Calculate cosine similarity between each pair of users
-similarity_df = pd.DataFrame(
-    index=ratings_df.columns, columns=ratings_df.columns)
-for i in similarity_df.columns:
-    for j in similarity_df.columns:
-        similarity_df.loc[i, j] = np.dot(ratings_df[i], ratings_df[j])/(
-            np.linalg.norm(ratings_df[i])*np.linalg.norm(ratings_df[j]))
+similarity_df2 = pd.DataFrame(
+    index=pivot_df.columns, columns=pivot_df.columns)
+for i in similarity_df2.columns:
+    for j in similarity_df2.columns:
+        similarity_df2.loc[i, j] = np.dot(pivot_df[i], pivot_df[j])/(
+            np.linalg.norm(pivot_df[i])*np.linalg.norm(pivot_df[j]))
+
 
 # Step 3: Identify neighbors
 # Select the top 2 most similar users as neighbors for each user
 k = 2
-neighbors_df = pd.DataFrame(index=ratings_df.columns, columns=range(1, k+1))
-for i in neighbors_df.index:
-    neighbor_series = similarity_df.loc[i].sort_values(ascending=False)[1:k+1]
-    neighbors_df.loc[i, :] = neighbor_series.index
+neighbors_df2 = pd.DataFrame(index=pivot_df.columns, columns=range(1, k+1))
+for i in neighbors_df2.index:
+    neighbor_series = similarity_df2.loc[i].sort_values(ascending=False)[1:k+1]
+    neighbors_df2.loc[i, :] = neighbor_series.index
 
 
-predicted_ratings_df = pd.DataFrame(
-    index=ratings_df.index, columns=ratings_df.columns)
-for i in predicted_ratings_df.index:
-    for j in predicted_ratings_df.columns:
-        if ratings_df.loc[i, j] == 0:
+# Step 4: Calculate Predicted Ratings
+# Calculate predicted ratings for unrated recipes based on similar users
+predicted_ratings_df2 = pd.DataFrame(
+    index=pivot_df.index, columns=pivot_df.columns)
+for i in predicted_ratings_df2.index:
+    for j in predicted_ratings_df2.columns:
+        if pivot_df.loc[i, j] == 0:
             numerator = 0
             denominator = 0
-            for neighbor in neighbors_df.loc[j]:
-                if ratings_df.loc[i, neighbor] != 0:
-                    numerator += similarity_df.loc[j,
-                                                   neighbor]*ratings_df.loc[i, neighbor]
+            for neighbor in neighbors_df2.loc[j]:
+                if pivot_df.loc[i, neighbor] != 0:
+                    numerator += similarity_df2.loc[j,
+                                                    neighbor]*pivot_df.loc[i, neighbor]
 
-                    denominator += similarity_df.loc[j, neighbor]
+                    denominator += similarity_df2.loc[j, neighbor]
 
             if denominator != 0:
-                predicted_ratings_df.loc[i, j] = numerator/denominator
+                predicted_ratings_df2.loc[i, j] = numerator/denominator
             else:
-                predicted_ratings_df.loc[i, j] = 0
+                predicted_ratings_df2.loc[i, j] = 0
+
 
 # Step 5: Recommend items
 # Recommend top 2 recipes with highest predicted ratings to each user
-recommendations_df = pd.DataFrame(
-    index=ratings_df.columns, columns=range(1, k+1))
-for i in recommendations_df.index:
-    recommended_series = predicted_ratings_df.loc[:, i].sort_values(ascending=False)[
+recommendations_df2 = pd.DataFrame(
+    index=pivot_df.columns, columns=range(1, k+1))
+for i in recommendations_df2.index:
+    recommended_series = predicted_ratings_df2.loc[:, i].sort_values(ascending=False)[
         :k]
-    recommendations_df.loc[i, :] = recommended_series.index
+    recommendations_df2.loc[i, :] = recommended_series.index
+
 
 print("Ratings Matrix:")
-print(ratings_df)
+print(pivot_df.to_string())
+
 print("\nSimilarity Matrix:")
-print(similarity_df)
+print(similarity_df2.to_string())
 print("\nNeighbors:")
-print(neighbors_df)
+print(neighbors_df2.to_string())
 print("\nPredicted Ratings:")
-print(predicted_ratings_df)
+print(predicted_ratings_df2.to_string())
 print("\nRecommendations:")
-print(recommendations_df)
+print(recommendations_df2.to_string())
+
+
+# define collums if rec df and convert them ti strings, then convert dataframe to dictionary
+recommendations_df2.columns = recommendations_df2.columns.astype(str)
+rec_dist = recommendations_df2.reset_index().to_dict(orient='records')
+
+# item_example = '6429903461a5315f99330597'
+
+# print(rec_dist[1])
+# if user id exists in the collection update recipe else insert the data into collection
+for rec_item in recommendations_df2.index:
+
+    item = collection3.find_one({"creator": rec_item})
+    if (item):
+        print("found")
+        item_dist = []
+        for i in rec_dist:
+            for key in i:
+                if rec_item == i[key]:
+                    item_dist = i
+        collection3.update_one({"creator": rec_item}, {"$set": item_dist})
+
+        # print("succsess")
+    # update the reccomended values
+    else:
+        print("notfound")
+        item_dist = []
+        for i in rec_dist:
+            for key in i:
+                if rec_item != i[key]:
+
+                    item_dist = i
+
+        collection3.insert_one(item_dist)
+
+    # add new user reccomended values
+
+# inserted_all = collection3.insert_many(rec_dist)
+# print(inserted_all.inserted_ids)
+# db.recommended.insert_many(recs_data_dict)
+
+
+# ----------------------------------------------------------------------------------------------------
